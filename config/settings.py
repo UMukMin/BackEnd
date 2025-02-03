3d2b74c17f6581861e81cbef759df6f2f724c277
wallace <j76067469@gmail.com>
2025-02-03 16:07:36 +0000

    Add restaurants app with basic routing and configuration

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import sys
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.restaurants',
 ]
 
 MIDDLEWARE = [
