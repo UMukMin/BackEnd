2d161ab9c74a23835ce0785abbf6e2e61a74595d
wallace <j76067469@gmail.com>
2025-03-10 08:27:48 +0000

    Fix : Remove PriceRange model and replace with price field in Restaurant model

--- api/restaurants/models/restaurants.py
+++ api/restaurants/models/restaurants.py
@@ -1,6 +1,5 @@
 from django.db import models
 from .categories import Category
-from .price_ranges import PriceRange
 
 class Restaurants(models.Model):
     idx = models.AutoField(primary_key=True)
@@ -11,7 +10,7 @@ class Restaurants(models.Model):
     description = models.TextField(null=True, blank=True)
     average_rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
     number_of_ratings = models.IntegerField(default=0)
-    price_range = models.ForeignKey(PriceRange, on_delete=models.SET_NULL, db_column='price_range_idx', null=True, blank=True)
+    price = models.CharField(max_length=10)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     reservation_available = models.BooleanField(default=False)
     opening_hours = models.TextField(null=True)
